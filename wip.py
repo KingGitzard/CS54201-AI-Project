cf38518b4652d94c0da1d349c62cb5694fbeb936
fredlongo <fred.a.longo@gmail.com>
2022-12-06 22:21:01 +0000

    Working Through PushPop

---
+++ wip.py
@@ -0,0 +1,11 @@
+
+
+DF_Stack = [[1,4,[[1,2],[3,4],[5,6]]],[1,5,[[1,2],[3,4],[5,6]]],[1,6,[[1,2],[3,4],[5,6]]]]
+# ord ,value, MapState
+
+
+print(DF_Stack)
+print ("len:", len(DF_Stack))
+del DF_Stack[-1]
+
+print(DF_Stack)
